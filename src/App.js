2294adb46e9e6777fab4450f225f6a36a372286d
Sahil Malik <maliksahil2580@gmail.com>
2025-04-06 18:58:07 -0400

    Updated homepage layout and optimized images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import logo from '../public/logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CDN Caching Demo</h1>
+      <img src={logo} alt="Logo" width="200" />
+      <p>This content should be cached by a CDN.</p>
     </div>
   );
 }
