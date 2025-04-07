6d71e3202cd626a5ab3fd0b8abe48efbe6db4cf9
Sahil Malik <maliksahil2580@gmail.com>
2025-04-06 22:08:58 -0400

    New File Updated Version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import logo from './public/Vanamo_Logo.png';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api/data')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('Fetched data:', data);
+        setData(data);
+      })
+      .catch((err) => console.error('Error fetching data:', err));
+  }, []);
+
+  
   return (
     <div className="App">
       <h1>CDN Caching Demo</h1>
